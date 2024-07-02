4f3da49e7dc5f3a8cc2daf0d44ce30fb25832263
pawanrock5756 <pawankumar7261048458@gmail.com>
2024-07-02 23:54:42 +0530

    initial commit

---
+++ BACKEND/database/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const dbConnection=()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName:"MERN_STACK_HOSPITAL_MANAGEMENT_SYSYTEM",
+    }).then(()=>{
+        console.log("connected to database!")
+    }).catch(err=>{
+        console.log("some error occured while connecting to database",err);
+    });
+};
